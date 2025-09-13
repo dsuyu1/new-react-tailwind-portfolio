a0323e53d566b1484d861e0c77fa881eebd90888
damian <34086682+dsuyu1@users.noreply.github.com>
2025-09-13 00:05:51 +0000

    edited hero and navbar sections

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,18 +12,18 @@ export const HeroSection = () => {
             <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1">
               {" "}
-              Pedro
+              Damian
             </span>
             <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
               {" "}
-              Machado
+              Villarreal
             </span>
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            I create stellar web experiences with modern technologies.
-            Specializing in front-end development, I build interfaces that are
-            both beautiful and functional.
+            I'm a student pursuing a career in cybersecurity.
+            While I focus on blue teaming and security operations,
+            I'm always exploring new areas of the field.
           </p>
 
           <div className="pt-4 opacity-0 animate-fade-in-delay-4">
