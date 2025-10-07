1cb1b68cf7b0edee3f60e4f2798048af882c03e0
damian <34086682+dsuyu1@users.noreply.github.com>
2025-10-07 14:11:12 +0000

    fix routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
-import Blog from "./pages/Blog";
 import TPOTArticle from "./pages/blog/tpot-writeup";
 import BBBBloatArticle from "./pages/blog/bbbbloat-writeup";
+import { ArticlesPage } from "./pages/Articles";
 
 function App() {
   return (
@@ -14,7 +14,6 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="articles" element={<ArticlesPage />} />
-          <Route path="blog" element={<Blog />} />
           <Route path="blog/tpot-writeup" element={<TPOTArticle />} />
           <Route path="blog/bbbbloat-writeup" element={<BBBBloatArticle />} />
           <Route path="*" element={<NotFound />} />
